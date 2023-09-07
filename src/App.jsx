70d78ab572dd66cc77f5227548bf16ce8c4dce77
Edwin Cheng <edwincheng09@googlemail.com>
2023-09-07 20:02:33 +0800

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from "../src/component/Nav"
 
 function App() {
-
   return(
-     <>
+    <>
+      <main className="website-background">
+        <Nav />
+      </main>
     </>
   )
 }
