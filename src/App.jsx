91f872c1c686c9d137c0409f33abf01d993e6558
Edwin Cheng <edwincheng09@googlemail.com>
2023-09-11 00:32:39 +0800

    added mouse event tracking

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import './App.css'
 import Nav from "../src/component/Nav"
 
 function App() {
+
+  document.addEventListener("mousemove", e => {
+    const xPosition = e.pageX
+    const yPosition = e.pageY
+    const target = document.querySelector("#mouse-event")
+    target.style.top = `${yPosition}px`
+    target.style.left = `${xPosition}px`
+  })
+
   return(
     <>
       <main>
         <Nav />
       </main>
+      <div id="mouse-event-container">
+        <div id="mouse-event"></div>
+      </div>
       <div className="bg"></div>
     </>
   )
--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -14,7 +14,7 @@ export default function Nav(){
           <a href="#"><li className="nav-link">Contact</li></a>
         </ul>
         <ul className="nav-contact-links">
-          <a href="#"><li className="nav-icon"><i class="fa fa-github-square" aria-hidden="true"></i></li></a>
+          <a href="#"><li className="nav-icon"><i className="fa fa-github-square" aria-hidden="true"></i></li></a>
         </ul>
       </nav>
     </>
