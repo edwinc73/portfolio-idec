2ded7f55ed351520678f44ba41ca520e38f530e1
Edwin Cheng <edwincheng09@googlemail.com>
2023-09-12 20:49:34 +0800

    started on hero & stack section

--- src/component/homepage/Home.jsx
+++ src/component/homepage/Home.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import "../homepage/Home.css"
 import Hero from "../homepage/Hero"
+import Stack from "../homepage/Stack"
 
 export default function Home(){
   return(
     <>
     <div className="wrapper">
       <Hero />
-      <Hero />
-      <Hero />
+      <Stack />
     </div>
     </>
   )
 }
+// need to work on hro spacing
